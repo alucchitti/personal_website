95c575891b7821039488184dd9bb6fc3a571d8b5
alucchitti <ael74@pitt.edu>
2021-07-23 16:31:49 -0400

    Update Photos

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -2,7 +2,11 @@ const Photos = () => {
     return (
         <div id='photos'>
             <h1>Latest Park Photos</h1>
-            
+            <div class="flex-container">
+                <div>Denali</div>
+                <div>Rocky Mountain</div>
+                <div>Kinzua</div>  
+            </div>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import Card from './Card'
+import {FaLink} from 'react-icons/fa'
 import movie from './images/movie.gif'
 import crossword from './images/crossword.jfif'
 
@@ -6,9 +7,9 @@ const Projects = () => {
     return (
         <div id='projects'>
             <h1>Latest Projects</h1>
-            <Card imgSrc={movie} alt={'Desc'} heading={"Who's Tweeting?"} subheading={'Python, Pandas, Django'} details={'A web app that predicts whether President Biden or Kim Kardashian is more likely to tweet user entered text. Uses ...'} date={'August 2021'}/>
-            <Card imgSrc={movie} alt={'Movie Demo'} heading={"What's that Movie?"} subheading={'HTML/CSS, JavaScript'} details={'Developed a web app using the OMDb API that allows users to search for movie titles and view movie details'} date={'June 2021'}/>
-            <Card imgSrc={crossword} alt={'Crossword Puzzle'} heading={'Cossword Puzzle Solver'} subheading={'Java'} details={'A program that recursively solves a crossword puzzle handling closed squares and predefined letters in a given board.'} date={'February 2021'}/>
+            <Card imgSrc={movie} alt={'Desc'} heading={"Who's Tweeting?"} subheading={'Python, Pandas, Django'} details={'A web app that predicts whether President Biden or Kim Kardashian is more likely to tweet user entered text. Uses ...'} date={<a href='https://github.com/alucchitti' target="_blank" rel="noopener noreferrer"><FaLink size={20}/></a>}/>
+            <Card imgSrc={movie} alt={'Movie Demo'} heading={"What's that Movie?"} subheading={'HTML/CSS, JavaScript'} details={'Developed a web app using the OMDb API that allows users to search for movie titles and view movie details'} date={<a href='https://alucchitti.github.io/MovieSearch/' target="_blank" rel="noopener noreferrer"><FaLink size={20}/></a>}/>
+            <Card imgSrc={crossword} alt={'Crossword Puzzle'} heading={'Cossword Puzzle Solver'} subheading={'Java'} details={'A program that recursively solves a crossword puzzle handling closed squares and predefined letters in a given board.'} date={''}/>
         </div>
     )
 }
