5217bffd1348120949864ce2daf6ac9882c55f77
alucchitti <ael74@pitt.edu>
2021-07-27 12:22:51 -0400

    Update styling

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -5,7 +5,7 @@ const Photos = () => {
     return (
         <div id='photos'>
             <h1 id='Photos'>Recent Park Photos</h1>
-            <p>One of my biggest hobbies is exploring the outdoors. Here are few pictures from my recent adventures!</p>
+            <p>One of my biggest hobbies is exploring the outdoors. Here are few pictures of my recent adventures!</p>
             <ImageSlider slides={SliderData}/>
         </div>
     )
