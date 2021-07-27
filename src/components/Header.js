5b4b2319535082d0f44afeb16f9a32be869d55e4
alucchitti <ael74@pitt.edu>
2021-07-27 10:35:10 -0400

    Update styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 const Header = () => {
     return (
         <div className='nav-bar'>
-            <h1 href="/Home"><span>Anthony Lucchitti</span></h1>
-            <h2 id="last" href="/Photos"><span>Photos</span></h2>
-            <h2 href="/Projects"><span>Projects</span></h2>
-            <h2 href="/Experience"><span>Experience</span></h2>
-            <h2 href="/Home"><span>Home</span></h2>
+            <h1 href="https://www.anthonylucchitti.com/"><span>Anthony Lucchitti</span></h1>
+            <h2 id="last" href="https://www.anthonylucchitti.com/#photos"><span>Photos</span></h2>
+            <h2 href="https://www.anthonylucchitti.com/#projects"><span>Projects</span></h2>
+            <h2 href="https://www.anthonylucchitti.com/#experience"><span>Experience</span></h2>
+            <h2 href="https://www.anthonylucchitti.com/"><span>Home</span></h2>
         </div>
     )
 }
