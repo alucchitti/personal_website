d98d927f63a5d2bcd33386018fd953f7e9cd2d67
alucchitti <ael74@pitt.edu>
2021-07-27 21:29:57 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import {FaGithubSquare, FaLinkedin, FaFilePdf} from 'react-icons/fa'
+import {FaPhoneAlt, FaGlobe} from 'react-icons/fa'
+import {GrMail} from 'react-icons/gr'
 
 const Home = () => {
     return (
@@ -9,6 +11,9 @@ const Home = () => {
                 <a href='https://github.com/alucchitti' target="_blank" rel="noopener noreferrer"><FaGithubSquare size={28}/></a>
                 <a href='https://example.com/' target="_blank" rel="noopener noreferrer"><FaFilePdf size={28}/></a>
                 <a href='https://www.linkedin.com/in/alucchitti/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={28}/></a>
+                <FaPhoneAlt />
+                <GrMail />
+                <FaGlobe />
             </div>
             <p>Hello! I am a junior Computer Science and Applied Statistics student at the University of Pittsburgh. I enjoy finding new ways to solve problems using technology. Feel free to explore my portfolio to see what I've been working on recently!</p>
         </div>
