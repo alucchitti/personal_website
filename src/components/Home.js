ed594cc75710b5518146687110d2428f2e923b7c
alucchitti <ael74@pitt.edu>
2021-07-28 16:35:39 -0400

    Update Resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import {FaGithubSquare, FaLinkedin, FaFilePdf} from 'react-icons/fa'
 import {RiMailFill} from 'react-icons/ri'
+import resume from './images/AnthonyLucchittiResume.pdf'
 
 const Home = () => {
     return (
@@ -8,7 +9,7 @@ const Home = () => {
             <h3>CS Student. Aspiring Developer. Data Enthusiast.</h3>
             <div id="icons">
                 <a href='https://github.com/alucchitti' target="_blank" rel="noopener noreferrer"><FaGithubSquare size={28}/></a>
-                <a href='https://example.com/' target="_blank" rel="noopener noreferrer"><FaFilePdf size={28}/></a>
+                <a href={resume} target="_blank" rel="noopener noreferrer"><FaFilePdf size={28}/></a>
                 <a href='mailto:alucchitti@pitt.edu'><RiMailFill size={28}/></a>
                 <a href='https://www.linkedin.com/in/alucchitti/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={28}/></a>
             </div>
