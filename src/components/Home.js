71f59881381baabe278b8349090235f855296b3e
alucchitti <ael74@pitt.edu>
2021-07-28 16:26:40 -0400

    Update Home.js
    Add email icon to home intro

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import {FaGithubSquare, FaLinkedin, FaFilePdf} from 'react-icons/fa'
+import {RiMailFill} from 'react-icons/ri'
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <div id="icons">
                 <a href='https://github.com/alucchitti' target="_blank" rel="noopener noreferrer"><FaGithubSquare size={28}/></a>
                 <a href='https://example.com/' target="_blank" rel="noopener noreferrer"><FaFilePdf size={28}/></a>
+                <a href='mailto:alucchitti@pitt.edu'><RiMailFill size={28}/></a>
                 <a href='https://www.linkedin.com/in/alucchitti/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={28}/></a>
             </div>
             <p>Hello! I am a junior Computer Science and Applied Statistics student at the University of Pittsburgh. I enjoy finding new ways to solve problems using technology. Feel free to explore my portfolio to see what I've been working on recently!</p>
