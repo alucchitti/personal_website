eec408c8c3197b947ef3553b3f5b42a28e89c6ba
alucchitti <ael74@pitt.edu>
2021-07-29 14:07:45 -0400

    Update Experience.js
    Update Cap1 Summit description

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,7 +7,7 @@ const Experience = () => {
     return (
         <div id='experience'>
             <h1 id='Experience'>Latest Experience</h1>
-            <Card imgSrc={cap1} alt={'Capital One logo'} heading={'Capital One'} subheading={'Software Engineering Summit'} details={'Invited to attend weeklong software engineering summit to attend workshops on ..., ..., ..., and ... led by Capital One Engineers.'} date={'August 2021'}/>
+            <Card imgSrc={cap1} alt={'Capital One logo'} heading={'Capital One'} subheading={'Software Engineering Summit Participant'} details={'Invited to take part in weeklong summit to attend workshops on hardware, android development, and machine learning led by Capital One Engineers.'} date={'August 2021'}/>
             <Card imgSrc={uss} alt={'U.S. Steel logo'} heading={'U.S. Steel'} subheading={'Data Science Intern'} details={'As a data science intern for U.S. Steel, I worked to improve the steelmaking process by predicting the future with machine learning models.'} date={'May 2021 - August 2021'}/>
             <Card imgSrc={pitt} alt={'University of Pittsburgh logo'} heading={'University of Pittsburgh'} subheading={'Student Athlete Tutor'} details={'As a data science intern for U.S. Steel, I worked to improve the steelmaking process by predicting the future with machine learning models.'} date={'August 2020 - April 2021'}/>
         </div>
