6620c3ab1b32d45230f11d181f6fa9b8edcc415d
alucchitti <ael74@pitt.edu>
2021-08-04 14:18:10 -0400

    Add tweet_predictor project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import Card from './Card'
 import {FaLink} from 'react-icons/fa'
+import tweet from './images/tweet.gif'
 import movie from './images/movie.gif'
 import crossword from './images/crossword.jfif'
 
@@ -7,7 +8,7 @@ const Projects = () => {
     return (
         <div id='projects'>
             <h1 id='Projects'>Latest Projects</h1>
-            {/* <Card imgSrc={movie} alt={'Desc'} heading={"Who's Tweeting?"} subheading={'Python, Pandas, Django'} details={'A web app that predicts whether President Biden or Kim Kardashian is more likely to tweet user entered text. Uses ...'} date={<a href='https://github.com/alucchitti' target="_blank" rel="noopener noreferrer"><FaLink size={20}/></a>}/> */}
+            <Card imgSrc={tweet} alt={'Desc'} heading={"Who's Tweeting?"} subheading={'Flask, React, scikit-learn'} details={'A web app that predicts whether former President Obama or Khloe Kardashian is more likely to tweet user entered text.'} date={<a href='https://alucchitti.github.io/tweet_predictor/' target="_blank" rel="noopener noreferrer"><FaLink size={20}/></a>}/>
             <Card imgSrc={movie} alt={'Movie Demo'} heading={"What's that Movie?"} subheading={'HTML/CSS, JavaScript'} details={'Developed a web app using the OMDb API that allows users to search for movie titles and view movie details'} date={<a href='https://alucchitti.github.io/MovieSearch/' target="_blank" rel="noopener noreferrer"><FaLink size={20}/></a>}/>
             <Card imgSrc={crossword} alt={'Crossword Puzzle'} heading={'Cossword Puzzle Solver'} subheading={'Java'} details={'A program that recursively solves a crossword puzzle handling closed squares and predefined letters in a given board.'} date={''}/>
         </div>
