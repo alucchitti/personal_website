cb1f2a10ba6f0fd3342784e15508e2f5744075e0
alucchitti <ael74@pitt.edu>
2022-09-22 14:00:12 -0400

    Remove resume and make small change to text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import {FaGithubSquare, FaLinkedin, FaFilePdf} from 'react-icons/fa'
 import {RiMailFill} from 'react-icons/ri'
-import resume from './images/AnthonyLucchittiResume.pdf'
 
 const Home = () => {
     return (
@@ -9,11 +8,10 @@ const Home = () => {
             <h3>CS Student. Aspiring Developer. Data Enthusiast.</h3>
             <div id="icons">
                 <a href='https://github.com/alucchitti' target="_blank" rel="noopener noreferrer"><FaGithubSquare size={28}/></a>
-                <a href={resume} target="_blank" rel="noopener noreferrer"><FaFilePdf size={28}/></a>
                 <a href='mailto:alucchitti@pitt.edu'><RiMailFill size={28}/></a>
                 <a href='https://www.linkedin.com/in/alucchitti/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={28}/></a>
             </div>
-            <p>Hello! I am a junior Computer Science and Applied Statistics student at the University of Pittsburgh. I enjoy finding new ways to solve problems using technology. Feel free to explore my portfolio to see what I've been working on recently!</p>
+            <p>Hello! I am a senior Computer Science and Applied Statistics student at the University of Pittsburgh. I enjoy finding new ways to solve problems using technology. Feel free to explore my portfolio to see what I've been working on recently!</p>
         </div>
     )
 }
