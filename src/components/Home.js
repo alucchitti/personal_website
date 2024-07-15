e12c32d9d748a1000a3c5dad25197a2c712ee921
Anthony Lucchitti <55965238+alucchitti@users.noreply.github.com>
2024-07-14 20:18:01 -0400

    lockfile and content updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ import {RiMailFill} from 'react-icons/ri'
 const Home = () => {
     return (
         <div id='home'>
-            <h1>Hi, I'm Anthony<span> (he/him)</span></h1>
-            <h3>CS Student. Aspiring Developer. Data Enthusiast.</h3>
+            <h1>Hi, I'm Anthony</h1>
+            <h3>Software Developer. Data Enthusiast. Aspiring Polymath.</h3>
             <div id="icons">
                 <a href='https://github.com/alucchitti' target="_blank" rel="noopener noreferrer"><FaGithubSquare size={28}/></a>
                 <a href='mailto:alucchitti@pitt.edu'><RiMailFill size={28}/></a>
                 <a href='https://www.linkedin.com/in/alucchitti/' target="_blank" rel="noopener noreferrer"><FaLinkedin size={28}/></a>
             </div>
-            <p>Hello! I am a senior Computer Science and Applied Statistics student at the University of Pittsburgh. I enjoy finding new ways to solve problems using technology. Feel free to explore my portfolio to see what I've been working on recently!</p>
+            <p>I'm a software developer based out of the Washington, D.C area. I enjoy finding new ways to solve problems using technology. Explore my portfolio to see what I've been working on recently!</p>
         </div>
     )
 }
